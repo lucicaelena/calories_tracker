6a18197d80c70dfeed16403b390aa47dbf03b16f
Scaunasu Lucica <scaunasu_lucicaelena@yahoo.com>
2024-03-19 18:08:11 +0200

    add navbar,welcome

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # Import all the flask related functions
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect
 # Import the database object which is initialized in the models module
 from models import db, Food
 from datetime import date
-
+from datetime import datetime
 # Initialize a Flask object on which we can write routes
 app = Flask(__name__)
 
@@ -12,9 +12,12 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///calories.db"
 
 # We initialize the database connection
 db.init_app(app)
-
-
 @app.route("/")
+def welcome():
+    return render_template("welcome.html")
+
+
+@app.route("/log_food")
 def home():
     day = request.args.get('day')
 
@@ -61,9 +64,26 @@ def home():
 
 @app.route("/add_food/", methods=["GET", "POST"])
 def add_food():
+    date_obj = datetime.now().date()
+    if request.method == 'GET':
+        return render_template('add_food.html')
+    elif request.method == 'POST':
+        food = Food(
+            name=request.form['name'],
+            meal=request.form['meal'],
+            calories=request.form['calories'],
+            carbs=request.form['carbs'],
+            proteins=request.form['proteins'],
+            fat=request.form['fat'],
+            date=date_obj
 
-    return render_template("add_food.html")
+        )
+        print(request.form['name'],)
+        print(request.form['meal'])
+        db.session.add(food)
+        db.session.commit()
+        return redirect('/')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
